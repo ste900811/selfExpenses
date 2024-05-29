03df611fe3ea89634562da4b321f5ab97fa83a03
Yiyang Lu <ste900811@gmail.com>
2024-05-29 11:37:31 -0400

    Initial Set up with TypeScript/Next.js

---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function About() {
+  return (
+    <div>
+      <Head>
+        <title>About Page</title>
+      </Head>
+      
+      <h1>About Page</h1>
+    </div>
+  );
+}
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Home Page</title>
+      </Head>
+      
+      <h1>Home Page</h1>
+    </div>
+  );
+}

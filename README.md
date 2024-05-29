7b0775d2f5e50590b42a8e091140ec2146d1b768
Yiyang Lu <ste900811@gmail.com>
2024-05-29 13:17:09 -0400

    Add activity Types section

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+Needto download MongoDB and install in "C:\Program Files\MongoDB\Server\[version]\[data/log]" directory
+
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,13 +1,16 @@
 import Head from "next/head";
+import activityTypes from "../datas/activityTypes";
 
 export default function About() {
+  const activities: any = activityTypes();
+
   return (
     <div>
       <Head>
         <title>About Page</title>
       </Head>
       
-      <h1>About Page</h1>
+      {activities.map((activity: String) => <p>{activity}</p>)}
     </div>
   );
 }
